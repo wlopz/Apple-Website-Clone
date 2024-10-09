b53544d4fce43a6c55672593dc99a0baea3632e3
Wilfredo <jrlopez85@gmail.com>
2024-10-09 03:01:44 -0500

    Features section added with animated videos, images and text

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,33 @@
+import React from 'react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+
+//...
+import * as Sentry from "@sentry/react";
+
+Sentry.init({
+  dsn: "https://7234bbb8035ad2563147c2d1b6b0c80e@o4508089723650048.ingest.us.sentry.io/4508089724829696",
+  integrations: [
+    Sentry.browserTracingIntegration(),
+    Sentry.reactRouterV6BrowserTracingIntegration({
+      useEffect: React.useEffect,
+    }),
+    Sentry.replayIntegration(),
+  ],
+  // Tracing
+  tracesSampleRate: 1.0, //  Capture 100% of the transactions
+  // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
+  tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+  // Session Replay
+  replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
+  replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+});
+
+// const container = document.getElementById(“app”);
+// const root = createRoot(container);
+// root.render(<App />);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/utils/animations.js
+++ src/utils/animations.js
@@ -1,7 +1,18 @@
 import gsap from "gsap"
-
 import { ScrollTrigger } from "gsap/all"
 gsap.registerPlugin(ScrollTrigger);
+
+export const animateWithGsap = (target, animationProps, scrollProps) => {
+  gsap.to(target, {
+    ...animationProps,
+    scrollTrigger: {
+      trigger: target,
+      toggleActions: 'restart reverse restart reverse',
+      start: 'top 85%',
+      ...scrollProps,
+    }
+  })
+}
 
 export const animateWithGsapTimeLine = (timeline, rotationRef, rotationState, firstTarget, secondTarget, animationProps) => {
   timeline.to(rotationRef.current.rotation, {
@@ -9,6 +20,7 @@ export const animateWithGsapTimeLine = (timeline, rotationRef, rotationState, fi
     duration: 1,
     ease: 'power2.inOut'
   })
+
   timeline.to(
     firstTarget,
     {
@@ -17,6 +29,7 @@ export const animateWithGsapTimeLine = (timeline, rotationRef, rotationState, fi
     },
     '<'
   )
+
   timeline.to(
     secondTarget,
     {
